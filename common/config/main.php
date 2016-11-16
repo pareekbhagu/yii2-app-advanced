6739202d6a0b1f35137f924784261c32d1a3961f
= <pareekbhagu@yahoo.com>
2016-11-16 15:51:03 +0530

    added main.php

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,23 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-    ],
+   
+
+	'db' =>
+[
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=ec2-54-75-233-92.eu-west-1.compute.amazonaws.com;port=5432;dbname=d6t1e8c0934ivn;', 
+    'username' => 'zbcpsidagsubix',
+    'password' => 'hDtx3ENIv-X3vyUS7T1X5GYpKj',
+    'charset' => 'utf8',
+    'schemaMap' => [
+      'pgsql'=> [
+        'class'=>'yii\db\pgsql\Schema',
+        'defaultSchema' => 'public' //specify your schema here
+      ]
+    ], // PostgreSQL
 ];
+
+ ],	
+	
+	];
